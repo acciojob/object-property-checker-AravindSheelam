dc4762a2cf4815897c037330a88c26a94647a514
Sheelam Aravind <102232179+AravindSheelam@users.noreply.github.com>
2023-07-17 21:44:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
+
+
   //   write your code here
-	const hasKey = sampleObject.hasOwnProperty(key);
-  
-  if(hasKey) console.log(true);
-  else console.log(false);
+	if (sampleObject.hasOwnProperty(key)) {
+		return true;
+	}
+	return false;
 }
 
 // Do not change the code below
